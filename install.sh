5723925ddb6fca191c510d9932bea658b5da2bb5
Victor Arribas <v.arribas.urjc@gmail.com>
2015-02-17 17:31:58 +0100

    add global gitignore (under ~)
    rewrite install script (for common lines)
    already integrated into gitconfig

--- install.sh
+++ install.sh
@@ -1,22 +1,33 @@
 #!/bin/sh
 
 
-name=.gitconfig
-file=$(pwd)/$name
+link_file(){
+name=$1
+source=$(pwd)/$name
 target=~/$name
 
-
 if [ -e "$target" ]; then
-	if [ "$1" = "-f" ]; then
+	if [ "$2" = "-f" ]; then
 		rm "$target"
 	else
 		echo "$name already exists. abort..."
-		exit 0
+		return 1
 	fi
 fi
 
-ln -s "$file" "$target"
+ln -s "$source" "$target"
 if [ $? -eq 0 ]; then
 	echo "$name installed successfully"
 fi
+}
 
+force_install=$1
+
+# install gitconfig
+name=.gitconfig
+link_file $name $force_install
+
+#install global ignore
+name=.gitignore_global
+link_file $name $force_install
+git config --global core.excludesfile \~/$name
